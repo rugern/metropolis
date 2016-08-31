b5aaa62516aff78b2b7e403e51ca6deeddabb38c
Nils Inge Rugsveen <nirugsveen@gmail.com>
2016-08-31 17:53:55 +0200

    Removed most of the old stuff, added new HDF5 data

--- autotrader.py
+++ autotrader.py
@@ -1,39 +1,20 @@
-import itertools
+import pandas
+import matplotlib.pyplot as pyplot
+from talib import abstract
 
-from sample import configuration
-from sample import engine as system
 
-# Unexpected keyword arguments
-#pylint: disable=E1123
-# Wrong hanging indentation
-#pylint: disable=C0330
+def main():
+    dataframes = pandas.read_hdf('result_15.h5')
 
-def main(config, plot):
-	engine = system.Engine()
-	engine.addStrategy(config['strategy'](config))
-	engine.addData(config['data'], config['startDate'], config['endDate'])
-	engine.addBroker(config['broker'])
-	engine.addSizer(config['sizer'])
-	engine.run()
-	if(plot): engine.plot()
-	return
+    buy_opens = dataframes[('buy', 'open')]
+    pyplot.plot(buy_opens)
 
-def optimize(config, plot):
-	values = [value for key, value in config['optimize'].items()]
-	for numbers in itertools.product(*values):
-		config = configuration.getConfigFile('parameters/parameters.json')
-		index = 0
-		for key in config['optimize']:
-			config[key] = numbers[index]
-			index += 1
-		main(config, plot)
+    test = dataframes['buy']
+    output = abstract.SMA(test, timeperiod=100)
+    pyplot.plot(output)
+
+    pyplot.show()
+
 
 if __name__ == '__main__':
-	config = configuration.getConfigFile('parameters/parameters.json')
-	print('Starting Portfolio Value: %.2f' % config['cash'])
-
-	mode = config['mode']
-	if(mode == 'optimize'):
-		optimize(config, config['plot'])
-	else:
-		main(config, config['plot'])
+    main()
