40bc8cdbdd160d2831001336fbf4b64967e210e1
Nils Inge Rugsveen <nirugsveen@gmail.com>
2017-03-19 19:32:02 +0100

    Working somewhat well with LSTM

--- bin/train.py
+++ bin/train.py
@@ -4,13 +4,14 @@ import os
 import math
 import numpy
 import random
-from numpy import random
 import pandas
-from keras.models import Sequential
-from keras.layers import Dense
+import h5py
+from sklearn.preprocessing import MinMaxScaler
+from keras.layers import Input, Dense, LSTM, Embedding
+from keras.models import Model
 from keras.optimizers import sgd
-import trading
 from bank import Bank
+
 
 def save(model, outputName):
     if outputName is None:
@@ -25,23 +26,45 @@ def normalize(raw):
     minimum = min(raw.min().values)
     return (raw - minimum) / (maximum - minimum)
 
-def createData(raw):
+def createTimesteps(data, lookback=2):
+    timesteps = data[0 : lookback].reshape((1, lookback, data.shape[1]))
+    for i in range(lookback * 2, data.shape[0]):
+        temp = data[i - lookback : i].reshape((1, lookback, data.shape[1]))
+        timesteps = numpy.append(timesteps, temp, axis=0)
+    return timesteps
+
+def saveToHdf(data, name):
+    output = h5py.File("results/" + name, "w")
+    output.create_dataset("data", data=data)
+    output.close()
+
+def createData(raw, scaler, lookback=2):
     # closePrices = data.iloc[:, 3].values
     # normalized = (closePrices - closePrices.mean()) / (closePrices.max() - closePrices.min())
     # indicators, longestPeriod = trading.createIndicators(normalized)
 
-    normalized = normalize(raw)
-    data = normalized.iloc[:-1, :].values
-    labels = normalized.iloc[1:, :].values
-    return data, labels
+    normalized = scaler.fit_transform(raw.values)
+    samples = normalized[:-1]
+    labels = normalized[1:]
+    samples = samples.reshape((-1, lookback, samples.shape[1]))
+    labels = labels[lookback - 1::lookback]
+    return samples, labels
 
-def getModel(features, inputName=None):
+def getModel(data, inputName=None):
     hiddenSize = 50
 
-    model = Sequential()
-    model.add(Dense(hiddenSize, input_dim=features, activation="relu"))
-    model.add(Dense(hiddenSize, activation="relu"))
-    model.add(Dense(features, activation="sigmoid"))
+    # model = Sequential()
+    # model.add(Dense(hiddenSize, input_dim=features, activation="relu"))
+    # model.add(Dense(hiddenSize, activation="relu"))
+    # model.add(Dense(features, activation="sigmoid"))
+    # model.compile(sgd(lr=0.2), "mse")
+
+    inputs = Input(shape=(data.shape[1], data.shape[2]))
+    x = LSTM(data.shape[2])(inputs)
+    x = Dense(hiddenSize, activation="relu")(x)
+    # x = Dense(hiddenSize, activation="relu")(x)
+    predictions = Dense(data.shape[2], activation="sigmoid")(x)
+    model = Model(inputs=inputs, outputs=predictions)
     model.compile(sgd(lr=0.2), "mse")
 
     if inputName is not None:
@@ -60,23 +83,33 @@ def findAction(estimate, price):
     return buy, sell
 
 # TODO: Test concatenation of features
-# TODO: Test LSTM
 # TODO: Test større nettverk (prøv gjerne å overfitte)
 # TODO: Transfer learning
 def train(model, data, labels):
-    model.fit(data, labels, epochs=10, batch_size=32)
+    model.fit(data, labels, epochs=1, batch_size=32)
 
-def test(model, data, raw):
+def test(model, data):
+    printIntervals = 10
+    modulo = data.shape[0] // printIntervals
+    predictions = model.predict(data[0].reshape(1, data.shape[1], data.shape[2])).reshape(1, -1)
+    print("Creating predictions...")
+    for i in range(1, data.shape[0]):
+        prediction = model.predict(data[i].reshape(1, data.shape[1], data.shape[2]))
+        predictions = numpy.append(predictions, prediction, axis=0)
+        if i % modulo == 0:
+            print("Progress: {}/{}".format(i, data.shape[0]))
+    return predictions
+    
+def marketTest(model, data, raw):
     startMoney = 10000
     bank = Bank(startMoney)
-    print("Starting value: {}".format(len(data)))
+    print("Data rows: {}".format(len(data)))
     buys = 0
     sells = 0
     holds = 0
     initialPrice = raw.iloc[0][3]
-    print(initialPrice)
 
-    for i in range(data.shape[0] // 5):
+    for i in range(data.shape[0]):
         estimate = model.predict(data[i].reshape(1 ,4))[0][3]
         price = raw.iloc[i][3]
         buy, sell = findAction(estimate, data[i][3])
@@ -104,6 +137,7 @@ def test(model, data, raw):
                   .format(buys, holds, sells))
 
     lastPrice = raw.iloc[-1][3]
+    print(initialPrice, lastPrice)
     holdValue = (10000 / initialPrice) * lastPrice
     total = bank.calculateValue(lastPrice)
     bound = total - bank.funds
@@ -116,41 +150,23 @@ def test(model, data, raw):
     print("Buys: {} | Holds: {} | Sells: {}"
           .format(buys, holds, sells))
 
-    # quota = 0.
-        # if (i - longestPeriod) % 100 == 0:
-            # progress = 100 * (i - longestPeriod) / (indicators.shape[0] - longestPeriod)
-            # holdValue = (10000 / closePrices[longestPeriod]) * closePrices[i]
-            # total = bank.calculateValue(closePrices[i])
-            # print("".join(["Progess: {:.2f}% | Price: ${:.5f} | Funds: ${:.2f} | ",
-                  # "Bound: ${:.2f} | Total: ${:.2f}| Hold: ${:.2f}"])
-                  # .format(progress, closePrices[i], bank.funds, total - bank.funds, total, holdValue))
-    # quota = prediction[action]
-    # reward = bank.performAction(closePrices[i], closePrices[i + 1], action, quota)
-
-    # holdValue = (10000 / closePrices[longestPeriod]) * closePrices[-1]
-    # total = bank.calculateValue(closePrices[-1])
-    # bound = total - bank.funds
-    # profit = 100 * (total - startMoney) / startMoney
-    # relativeProfit = 100 * (total - holdValue) / holdValue
-    # print("Loss: {:.4f}".format(loss))
-    # print("".join(["Funds: ${:.2f} | Bound: ${:.2f} | Total: ${:.2f} | Hold: ${:.2f} | ",
-          # "Relative profit: {:.2f}% | Profit: {:.2f}%"])
-          # .format(bank.funds, bound, total, holdValue, relativeProfit, profit))
-    # print("Buys: {} | Holds: {} | Sells: {}"
-          # .format(actionsPerformed[2], actionsPerformed[1], actionsPerformed[0]))
-
 if __name__ == "__main__":
     inputName = "model/testmodel"
     outputName = "model/testmodel"
 
-    model = getModel(4, inputName)
+    scaler = MinMaxScaler(feature_range=(0, 1))
     raw = pandas.read_hdf("data/EUR_BITCOIN_2016/krakenEUR_2016_padded.hdf5")
-    data, labels = createData(raw)
-    ratio = round(len(data) * 75/100)
-    training_data = data[:ratio]
+    samples, labels = createData(raw, scaler)
+    ratio = len(samples) * 75 // 100
+    training_data = samples[:ratio]
     training_labels = labels[:ratio]
+    testData = samples[ratio:]
+    testLabels = labels[ratio:]
 
+    model = getModel(samples, inputName)
     # train(model, training_data, training_labels)
     # save(model, outputName)
 
-    test(model, data, raw)
+    predictions = test(model, testData)
+    saveToHdf(scaler.inverse_transform(predictions), "predictions.h5")
+    saveToHdf(scaler.inverse_transform(testLabels), "labels.h5")
--- tools/plot.py
+++ tools/plot.py
@@ -1,21 +1,43 @@
 import os
 import sys
-import pandas
+import h5py
 from matplotlib import pyplot
 
-def plot(inputName):
-    data = pandas.read_hdf(inputName).dropna()
+def assertExists(name):
+    if not os.path.isfile(name):
+        print("Could not find datafile: {}".format(name))
+        sys.exit(1)
+
+def readHdf(name):
+    infile = h5py.File(name, "r")
+    data = infile["data"][:][:, 3]
+    infile.close()
+    return data
+
+def plotSingle(args):
+    name = args[1]
+    data = readHdf(name)
     pyplot.plot(data)
+    pyplot.show()
+
+def plotDouble(args):
+    first, second = args[1:]
+    assertExists(first)
+    assertExists(second)
+    firstData = readHdf(first)
+    secondData = readHdf(second)
+    firstPlot = pyplot.plot(firstData, label=first)
+    secondPlot = pyplot.plot(secondData, label=second)
+    pyplot.legend()
     pyplot.show()
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Datafile required")
         sys.exit(1)
-
-    inputName = sys.argv[1]
-    if not os.path.isfile(inputName):
-        print("Could not find datafile: {}".format(inputName))
-        sys.exit(1)
-
-    plot(inputName)
+    elif len(sys.argv) == 2:
+        plotSingle(sys.argv)
+    elif len(sys.argv) == 3:
+        plotDouble(sys.argv)
+    else:
+        print("Does not support so many arguments")
