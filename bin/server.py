154c1a6656b08b39daaa81b923e35bb2bcb72987
Nils Inge Rugsveen <nirugsveen@gmail.com>
2017-03-31 08:15:08 +0200

    Restructuring and fixing server

--- bin/server.py
+++ bin/server.py
@@ -1,19 +1,31 @@
+import h5py
 import json
+import time
 from os import listdir
 from os.path import isfile, join
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
+from flask_socketio import SocketIO, emit
+import eventlet
 
+eventlet.monkey_patch()
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
+socket = SocketIO(app)
 
 successResponse = {
     "success": True,
     "text": "The operation was successful",
     "statusCode": 200
 }
-activeConfig = "";
 
+def readHdf(name):
+    infile = h5py.File(name, "r")
+    data = infile["data"][:]
+    if len(data.shape) > 1:
+        data = data[:, 3]
+    infile.close()
+    return data
 
 def readJson(path):
     data = None
@@ -21,48 +33,39 @@ def readJson(path):
         data = json.load(infile)
     return data
 
-
 def writeJson(path, data):
     with open(path, "w") as outfile:
         json.dump(data, outfile)
-
 
 def getDirectoryList(path):
     filenames = [name for name in listdir(path) if isfile(join(path, name))]
     return filenames
 
+# @app.route("/run", methods=["POST"])
+# def run():
+    # return jsonify(successResponse)
 
-@app.route("/configs", methods=["GET", "POST"])
-def configuration():
-    configPath = None
-    if "name" in request.args:
-        activeConfig = request.args["name"]
-        configPath = "./config/" + activeConfig
+# @app.route("/results", methods=["GET"])
+# def getResults():
+    # if "name" in request.args:
+        # result = readHdf("results/{}".format(request.args["name"])).tolist()
+        # return jsonify(result)
+    # resultNames = getDirectoryList("results/")
+    # return jsonify(resultNames)
 
-    if request.method == "POST":
-        writeJson(configPath, request.get_json())
-        return jsonify(successResponse)
+@socket.on("connect")
+def connected():
+    print("Client connected")
 
-    if configPath is None:
-        return jsonify({ "configs": getDirectoryList("./config") })
-    return jsonify(readJson(configPath))
+@socket.on("plots")
+def emitPlot():
+    emit("plots", getDirectoryList("results/"))
 
-
-@app.route("/run", methods=["POST"])
-def run():
-    return jsonify(successResponse)
-
-
-@app.route("/save", methods=["POST"])
-def save():
-    return jsonify(successResponse)
-
-
-@app.route("/reset", methods=["POST"])
-def reset():
-    return jsonify(successResponse)
-
+@socket.on("plot")
+def emitPlotData(data):
+    print(data)
+    emit("plot", readHdf("results/{}".format(data)).tolist())
 
 if __name__ == "__main__":
-    app.run()
+    socket.run(app)
 
--- bin/trading.py
+++ bin/trading.py
@@ -3,13 +3,16 @@ import talib
 
 
 def createIndicators(prices):
+    openPrice, highPrice, lowPrice, closePrice = prices[:]
     # sma = talib.SMA(prices)
-    ema = talib.EMA(prices[:, 3], timeperiod=20)
+    ema = talib.EMA(closePrice, timeperiod=20)
     # macd, macdsignal, macdhist = talib.MACD(prices, fastperiod=12, slowperiod=26, signalperiod=9)
-    upperband, middleband, lowerband = talib.BBANDS(prices[:, 3], timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
-    rsi = talib.RSI(prices[:, 3], timeperiod=14)
+    upperband, middleband, lowerband = talib.BBANDS(closePrice, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
+    rsi = talib.RSI(closePrice, timeperiod=14)
+    adx = talib.ADX(highPrice, lowPrice, closePrice, timeperiod=14)
+    slowk, slowd = STOCH(highPrice, lowPrice, closePrice, fastk_period=5, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)
     
     # return numpy.column_stack((sma, real, macd, macdsignal, macdhist, upperband, middleband, lowerband)), longestPeriod
-    return numpy.column_stack((prices, ema, rsi, upperband, middleband, lowerband)), 20
+    return numpy.column_stack((prices, ema, rsi, upperband, middleband, lowerband, adx, slowk, slowd)), 20
 
 
